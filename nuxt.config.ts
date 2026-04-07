1b043a792d1b0c1d8b147fa4ee9986e94dc74225
Kirin Nakayama <nakayama-kirin@users.noreply.github.com>
2026-04-07 23:50:32 +0900

    Vuetify Setup / ソース管理を `src` 以下に

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  srcDir: 'src/',
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    plugins: [
+      // @ts-expect-error
+      vuetify({ autoImport: true }),
+    ],
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })

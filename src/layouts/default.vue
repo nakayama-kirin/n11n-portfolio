b1a3e79661ceafbcf7855de5e8a08bdc51a61f8b
Kirin Nakayama <nakayama-kirin@users.noreply.github.com>
2026-04-10 20:26:47 +0900

    Google Fontsを組み込み

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -6,4 +6,22 @@
 
 <script setup lang="ts">
 // Vuetify layout provider
+useHead({
+  link: [
+    { rel: 'preconnect', href: '//fonts.googleapis.com' },
+    { rel: 'preconnect', href: '//fonts.gstatic.com', crossorigin: '' },
+    { 
+      rel: 'stylesheet', 
+      href: '//fonts.googleapis.com/css2?family=Playwrite+IE:wght@100..400&display=swap' 
+    }
+  ]
+})
 </script>
+
+<style>
+.font-playwrite {
+  font-family: "Playwrite IE", cursive;
+  font-optical-sizing: auto;
+  font-style: normal;
+}
+</style>

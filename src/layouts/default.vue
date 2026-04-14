057a78677d6908efdf830e9f527b7728f043bc1d
Kirin Nakayama <nakayama-kirin@users.noreply.github.com>
2026-04-15 00:20:40 +0900

    Google Fonts設定追加およびデフォルトスタイルの調整

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -12,15 +12,39 @@ useHead({
     { rel: 'preconnect', href: '//fonts.gstatic.com', crossorigin: '' },
     { 
       rel: 'stylesheet', 
-      href: '//fonts.googleapis.com/css2?family=Playwrite+IE:wght@100..400&display=swap' 
+      href: '//fonts.googleapis.com/css2?family=Google+Sans+Code:ital,wght@0,300..800;1,300..800&family=Noto+Sans+JP:wght@100..900&family=Playwrite+IE:wght@100..400&display=swap' 
     }
   ]
 })
 </script>
 
 <style lang="scss">
+html {
+  font-family: "Noto Sans JP", sans-serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+  font-size: 10px;
+}
+
+.l-container {
+  position: relative;
+  display: block;
+  overflow: hidden;
+  width: 100%;
+  min-height: 100vh;
+  font-size: 1.6rem;
+  color: map.get($font-color, light);
+  background-color: map.get($theme, light);
+}
+
 .font-playwrite {
   font-family: "Playwrite IE", cursive;
+  font-optical-sizing: auto;
+  font-style: normal;
+}
+
+.font-google-sans-code {
+  font-family: "Google Sans Code", monospace;
   font-optical-sizing: auto;
   font-style: normal;
 }

2226e7bfbff6ce5d20d434b7d84584a62cb33ad3
Kirin Nakayama <nakayama-kirin@users.noreply.github.com>
2026-04-15 00:25:59 +0900

    メインビジュアル箇所の作成

---
+++ src/components/Inner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="l-inner">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+.l-inner {
+  width: 100%;
+  padding: 0 map.get($padding, sp);
+  margin: 0 auto;
+
+  @include media(m) {
+    padding-inline: map.get($padding, tb);
+  }
+
+  @include media(l) {
+    max-width: $inner-max;
+    padding-inline: map.get($padding, pc);
+  }
+
+  >* {
+    &:first-child {
+      margin-top: 0 !important;
+    }
+
+    &:last-child {
+      margin-bottom: 0 !important;
+    }
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,10 +2,17 @@
   <main>
     <div class="hero">
       <div class="hero-content">
-        <h1 class="font-playwrite">Portfolio</h1>
+        <Inner>
+          <h1 class="hero-heading font-playwrite">Portfolio</h1>
+          <p class="hero-paragraph">
+            Nakayama Kirin / Frontend Engineer
+          </p>
+        </Inner>
       </div>
       <div class="hero-bg">
-        <GeometricAnimation :ratio="[16, 8]" />
+        <div class="hero-bg-inner">
+          <GeometricAnimation :ratio="[16, 8]" />
+        </div>
       </div>
     </div>
   </main>
@@ -31,6 +38,11 @@
     z-index: 1;
     inset: 0;
     margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    text-shadow: 0 0 1em rgba(map.get($theme, light), 0.7);
   }
   
   &-bg {
@@ -38,8 +50,61 @@
     z-index: 0;
     opacity: 0.7;
     width: 100%;
-    max-width: map-get($media-pc, max);
     margin: 0 auto;
+
+    @include media(l) {
+      display: flex;
+      justify-content: center;
+    }
+    
+    &-inner {
+      @include media(l) {
+        position: relative;
+        flex: 0 0 auto;
+        width: map.get($media-pc, max);
+      }
+
+      &::before,
+      &::after {
+        @include media(l) {
+          content: "";
+          display: block;
+          position: absolute;
+          z-index: 1;
+          inset: 0;
+          width: map.get($padding, pc) * 2;
+          background: linear-gradient(to left, transparent 0%, rgba(map.get($theme, light), 1) 90%);
+          user-select: none;
+        }
+      }
+
+      &::before {
+        @include media(l) {
+          right: unset;
+        }
+      }
+
+      &::after {
+        @include media(l) {
+          left: unset;
+          transform: scaleX(-1);
+        }
+      }
+    }
+  }
+
+  &-heading {
+    margin: 1em 0 0.5em;
+    font-size: 3.6rem;
+    line-height: 1.2;
+
+    @include media(m) {
+      font-size: 4.8rem;
+    }
+
+    @include media(l) {
+      font-size: 6rem;
+    }
   }
 }
 </style>
